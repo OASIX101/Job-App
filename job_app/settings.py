5a733eab2db4378be7977203439aa5752776ec75
unknown <anthonyolowuxx6@gmail.com>
2022-09-21 13:07:42 +0100

    reset settings

--- job_app/settings.py
+++ job_app/settings.py
@@ -6,7 +6,7 @@ from django.utils.timezone import timedelta
 load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
