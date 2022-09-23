278e40a49971bc12abb495c5ab67d80923bad5de
unknown <anthonyolowuxx6@gmail.com>
2022-09-23 13:23:33 +0100

    (#01)update

--- job_app/settings/base.py
+++ job_app/settings/base.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'job_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME')
+        'NAME': os.getenv('NAME'),
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
         'HOST': os.getenv('HOST'),
